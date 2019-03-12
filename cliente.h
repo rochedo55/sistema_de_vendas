65037c012facdc5fa6a24c00428487216d7fb71f
rochedo55 <43376647+rochedo55@users.noreply.github.com>
2019-03-12 09:52:02 -0300

    Create cliente.h

---
+++ cliente.h
@@ -0,0 +1,14 @@
+#ifndef cliente_H
+#define cliente_H
+
+
+using namespace std;
+
+class cliente{
+	string nome_cliente;
+	string numero;
+	string email;
+	void metodo_cadastrar(/*argumentos para cadastrar*/);
+};
+
+#endif
